73f02aefc5fd4bfbd200544356d9c316c3b0bfc4
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-06-16 12:44:53 -0600

    area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-""" Module Sqaure """
-
-
 class Square:
-    """ Square class defined by geometric shap
-
-        Attributes:
-            size (int): Size of square
+    """ A class that defines a square by its size
     """
     def __init__(self, size=0):
+        """ Method to initialize the square object
         """
-        initializes the square
-        Args:
-            size (int): size of a side of the square
-        Raises:
-            TypeError: if size is not int
-            ValueError: size less than 0
-        Returns:
-            None
-        """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-                raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
     def area(self):
+        """ Method that returns the square area of the object
         """
-            set square square area
-
-            Return:
-                the current square area (int)
-        """
-        return self.__size ** 2
+        return (self.__size ** 2)
