277b51b8058b20654e8a32c12355be804cd3aa49
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-06-16 13:18:43 -0600

    all exercises

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """
-    Add integer
+
+Add Integer or Float Module
+
 """
 
 
 def add_integer(a, b=98):
-    """ add two integers or flots """
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return (a + b)
+    """
+    ADD Two integer a and b
+
+    Args:
+        a (int/float): first int
+        b (int/float): Second int
+
+    Raises:
+        TypeError: in case the arguments are not int or float
+
+    Return:
+        (int) : Sum of the int a and b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    if (a + b) == float('inf') or (a + b) == -float('inf'):
+        return b
+    return int(a) + int(b)
