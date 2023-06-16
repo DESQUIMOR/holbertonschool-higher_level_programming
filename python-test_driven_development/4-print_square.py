38e92b915a04d489096abdd6a84e1301a44b266c
Diego Esquivel <desquimor@Hogwarts.localdomain>
2023-06-16 14:50:24 -0600

    print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-""" """
+"""the print square module"""
 
 
 def print_square(size):
+     """prints a square of #
+
+    Args:
+        size: the size in int of one side of the square
+    Raises:
+        TypeError: if size != int
+        ValueError: if size < 0
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
