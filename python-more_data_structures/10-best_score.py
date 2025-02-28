bdd995b72a259082c20cfedbbc92701ff75dec7f
Diego Esquivel <desquimor@Hogwarts>
2025-02-28 14:23:23 -0600

    fix pycodestyle

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    """Returns the key with the highest integer value in a dictionary."""
     if a_dictionary:
-        a = sorted((value, key) for key, value in a_dictionary.items()).pop()[1]
-        return a
+        sorted_items = sorted((value, key) for key, value in a_dictionary.items())
+        return sorted_items.pop()[1]
