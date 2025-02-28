86410ea6a411cae1d3fc9d656d0acd1ddb62d582
Diego Esquivel <desquimor@Hogwarts>
2025-02-28 14:25:01 -0600

    fix pycodestyle

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        sorted_items = sorted((value, key) for key, value in a_dictionary.items())
-        return sorted_items.pop()[1]
+    if not a_dictionary:
+        return None
+
+    sorted_items = sorted((v, k) for k, v in a_dictionary.items())
+    return sorted_items[-1][1]
