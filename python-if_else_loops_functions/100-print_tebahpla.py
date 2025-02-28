ee398ed35fbe09f1e5ea989715a1db9a5c0289ba
Diego Esquivel <desquimor@Hogwarts>
2025-02-28 12:56:57 -0600

    fit pythonstyle

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(c - (32 if c % 2 == 1 else 0))) for c in range(122, 96, -1)), end="")
+print("".join(
+    "{}".format(chr(c - (32 if c % 2 == 1 else 0)))
+    for c in range(122, 96, -1)
+), end="")
