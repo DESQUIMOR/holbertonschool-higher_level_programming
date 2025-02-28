ffd205b7b7d1783fa330492293b09849faea7a02
Diego Esquivel <desquimor@Hogwarts>
2025-02-28 15:55:53 -0600

    fix 101-lazy_matrix_mul.py

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -19,4 +19,18 @@ def lazy_matrix_mul(m_a, m_b):
     Raises:
         ValueError: If matrices cannot be multiplied
     """
-    return np.matmul(m_a, m_b)
+    try:
+        result = np.matmul(m_a, m_b)
+        return result
+    except ValueError as e:
+        if "shapes" in str(e):
+            raise ValueError(str(e))
+        else:
+            raise ValueError("Scalar operands are not allowed, use '*' instead")
+    except TypeError as e:
+        if "invalid data type" in str(e):
+            raise TypeError("invalid data type for einsum")
+        elif "object of type" in str(e) and "has no len" in str(e):
+            raise TypeError(str(e))
+        else:
+            raise TypeError("Scalar operands are not allowed, use '*' instead")
