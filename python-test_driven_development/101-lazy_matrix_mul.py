b228df5ab6f349f5a72675ad0c74d5a76233c424
Diego Esquivel <desquimor@Hogwarts>
2025-02-28 15:28:02 -0600

    101-lazy_matrix_mul.py

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -20,9 +20,23 @@ def lazy_matrix_mul(m_a, m_b):
         TypeError: If m_a or m_b is not a list of lists of integers/floats.
         ValueError: If m_a or m_b is empty or if they cannot be multiplied.
     """
+    if not isinstance(m_a, list) or not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not isinstance(m_b, list) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("m_b should contain only integers or floats")
+    if not m_a or not any(m_a):
+        raise ValueError("m_a can't be empty")
+    if not m_b or not any(m_b):
+        raise ValueError("m_b can't be empty")
+    if len(set(len(row) for row in m_a)) > 1:
+        raise TypeError("each row of m_a must be of the same size")
+    if len(set(len(row) for row in m_b)) > 1:
+        raise TypeError("each row of m_b must be of the same size")
     try:
         return np.matmul(m_a, m_b).tolist()
     except ValueError:
         raise ValueError("m_a and m_b can't be multiplied")
-    except TypeError:
-        raise TypeError("Both matrices must be lists of lists of integers or floats")
