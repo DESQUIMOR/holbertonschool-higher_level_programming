de3fe76048144f4839ff9abfb90ae5589565b5fe
Diego Esquivel <desquimor@Hogwarts>
2025-03-13 12:32:15 -0600

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-A Student class that defines a Studen (module)
+A Student class that defines a Student (module)
 """
 
 
-class Student():
+class Student:
     """
-        A Student class that defines a Student
+    A Student class that defines a Student
     """
     def __init__(self, first_name, last_name, age):
         """
-            INIT
+        INIT
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -18,15 +18,17 @@ class Student():
 
     def to_json(self, attrs=None):
         """
-            retrieves a dictionary representation
-            of a Student instance
+        Retrieves a dictionary representation
+        of a Student instance
         """
-        if type(attrs) is list and all([type(x) == str for x in attrs]):
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return (self.__dict__)
+        return self.__dict__
 
     def reload_from_json(self, json):
         """
+        Replaces attributes of the Student instance
+        with values from a dictionary
         """
-        for i, j in json.items():
-            self.__dict__[i] = j
+        for key, value in json.items():
+            setattr(self, key, value)
