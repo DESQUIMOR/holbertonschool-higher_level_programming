26181185451cd867d83b03d86efb1fadef7b1ac6
Diego Esquivel <desquimor@Hogwarts>
2025-03-28 17:44:50 -0600

    13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
--- List all genres and the number of shows linked to each
-SELECT genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY genres.name
+-- Lists all genres with the number of shows linked
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
